b0742ab47b4a85b41b97863c1d8de2e9115e1839
Ethan Motion <ethan.motion@xero.com>
2020-06-18 20:12:45 +1200

    Add CloudFront invalidation step, and point www. to ethanmotion.com distro

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import mimetypes
 import boto3
 import logging
@@ -26,41 +27,64 @@ s3_client = boto3.client('s3',
     aws_secret_access_key = aws_secret_access_key
 )
 
+cloudfront_client = boto3.client('cloudfront',
+    aws_access_key_id = aws_access_key_id,
+    aws_secret_access_key = aws_secret_access_key
+)
+
 def main():
     source = input("Local or Git: ")
     environment = input("Dev or Prod: ")
     if environment.lower() == "prod":
-        buckets = ['ethanmotion.com', 'www.ethanmotion.com']
+        bucket_name = 'ethanmotion.com'
+        distribution = 'EM30Y9BEWP31R'
     else:
-        buckets = ['dev.ethanmotion.com']
+        bucket_name = 'dev.ethanmotion.com'
+        distribution = 'E2N0SDO3M868R'
 
     if source.lower() == "local":
         logger.info("Using local")
         os.chdir('website')
     else:
-        logger.info("Using git master")
+        logger.info("Can't use git master yet")
+        sys.exit()
         # make a tmp dir, and clean up after
         # logging.info(f"Cloning master from {repo}")
         # os.system("git clone " + repo)
         # os.chdir('./ethanmotion.com/website')
 
-    # Delete everything from bucket(s)
-    for bucket_name in buckets:
-        logger.info(f"Deleting objects from {bucket_name}")
-        bucket = s3_resource.Bucket(bucket_name)
-        bucket.objects.delete()
+    # Delete everything from bucket
+    logger.info(f"Deleting objects from {bucket_name}")
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.objects.delete()
 
     # Upload files to bucket(s)
-        for file in Path().rglob('*.*'):
-            content_type = mimetypes.guess_type(file)
-            logger.info(f"Uploading {file} {content_type[0]}")
-            content = open(file, 'rb')
-            s3_client.put_object(
-                Bucket=bucket_name,
-                Body=content,
-                Key=str(file.as_posix()),
-                ContentType=str(content_type[0])
-            )
+    for file in Path().rglob('*.*'):
+        content_type = mimetypes.guess_type(file)
+        logger.info(f"Uploading {file} {content_type[0]}")
+        content = open(file, 'rb')
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Body=content,
+            Key=str(file.as_posix()),
+            ContentType=str(content_type[0])
+        )
+
+    # Invalidate CloudFront cached files
+    logger.info(f"Invalidating cache for distribution {distribution}")
+    cloudfront_client.create_invalidation(
+        DistributionId=distribution,
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': 1,
+                'Items': [
+                    '/*'
+                ]
+            },
+            'CallerReference': 'it_me'
+        }
+    )
+
 
 if __name__ == '__main__':
     main()
